c6fe9bacd3eb5be34bb83789b6fff1d13cbf83e9
Andre <aanuta9997@gmail.com>
2023-04-12 12:24:39 +0200

    fixed exercise 10

--- javascript/es-10-javascript/exercise-2.js
+++ javascript/es-10-javascript/exercise-2.js
@@ -6,12 +6,15 @@ const order = {
   console.log('City is required');
 } */
 
+let definedCity = order.customer?.addres?.city ? "city is ok" : "city is required"
+console.log(definedCity);
 
 
-for (let city in order) {
+
+/* for (let city in order) {
   if (order[city]){
     console.log(order[city]);
   } else {
     console.log('city is required');
   }
-}
+} */
