278f99939992d3a8ad295c57c8707b21c31e9693
Andre <aanuta9997@gmail.com>
2023-04-23 17:54:10 +0200

    javascript destructuring exercise 23

---
+++ javascript/es-23/exercise-3.js
@@ -0,0 +1,14 @@
+function isAdult() {
+  return age >= 18;
+};
+
+const person = {
+  id: 1,
+  firstName: 'Mario',
+  lastName: 'Rossi',
+  age: 25
+};
+
+let {age} = person;
+
+console.log(isAdult(person));
