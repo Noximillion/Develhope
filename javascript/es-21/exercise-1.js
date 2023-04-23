031380b2f1bed22465171b7fc0ec4b06453b2ae6
Andre <aanuta9997@gmail.com>
2023-04-23 17:51:08 +0200

    javascript destructuring exercise 21

---
+++ javascript/es-21/exercise-1.js
@@ -0,0 +1,15 @@
+let [a,b] = [10, 20];
+
+let num1 = a;
+let num2 = b;
+console.log('Before swap: ', num1, num2); // Before swap: 10 20
+
+// Variable swapping
+num2 = a;
+num1 = b;
+
+console.log('After swap: ', num1, num2); // After swap: 20 10 
+
+
+
+ 
