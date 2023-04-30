a8374e73da92546ed097fc9594795fea55f32dcb
Andre <aanuta9997@gmail.com>
2023-04-30 17:06:39 +0200

    corrected exercise 21

--- javascript/es-21/exercise-1.js
+++ javascript/es-21/exercise-1.js
@@ -1,15 +1,21 @@
-let [a,b] = [10, 20];
+// let num1 = 10;
+// let num2 = 20;
+// console.log('Before swap: ', num1, num2); // Before swap: 10 20
 
-let num1 = a;
-let num2 = b;
+// // Variable swapping
+// let temp = num2;
+// num2 = num1;
+// num1 = temp;
+
+// console.log('After swap: ', num1, num2); // After swap: 20 10
+
+
+
+let num1 = 10;
+let num2 = 20;
 console.log('Before swap: ', num1, num2); // Before swap: 10 20
 
 // Variable swapping
-num2 = a;
-num1 = b;
+[num2, num1] = [num1, num2]
 
 console.log('After swap: ', num1, num2); // After swap: 20 10 
-
-
-
- 
