75257a2633f8266a0dabda12fe3d9c67c8fbf87e
Andre <aanuta9997@gmail.com>
2023-05-06 20:09:40 +0200

    javascript promise exercise 37

---
+++ javascript/es-37/exercise.js
@@ -0,0 +1,16 @@
+const number = 15;
+
+
+let promise = new Promise((resolve, reject) => {
+    if(number > 10){
+        resolve(number)
+    } else {
+        reject(number)
+    }
+})
+
+
+promise
+    .then((val) => console.log(val))
+.catch((err) => console.error(err))
+
