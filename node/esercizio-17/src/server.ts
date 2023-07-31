9e3147c1cd9365e2f9e2b9bb90f469ec5a1a0ae5
Andre <aanuta9997@gmail.com>
2023-07-31 20:14:14 +0200

    esercizio 17

---
+++ node/esercizio-17/src/server.ts
@@ -0,0 +1,54 @@
+import express from "express";
+import "express-async-errors";
+import morgan from "morgan";
+import {
+    getAll,
+    getOneById,
+    create,
+    updateById,
+    deleteById,
+    addImage,
+} from "./controllers/planets";
+import { logOut, login, signUp } from "./controllers/users";
+import multer from "multer"
+import { authorize } from "./authorize";
+import "./passport";
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "./uploads")
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({storage})
+
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+app.use(morgan("dev"));
+
+app.get("/api/planets", getAll);
+
+app.get("/api/planets/:id", getOneById);
+
+app.post("/api/planets", create);
+
+app.put("/api/planets/:id", updateById);
+
+app.delete("/api/planets/:id", deleteById);
+
+app.post("/api/planets:id/image", upload.single('image'), addImage);
+
+app.post("/api/users/login", login)
+
+app.post("/api/users/signup", signUp)
+
+app.get("/api/users/logout", authorize, logOut)
+
+app.listen(port, () => {
+    console.log(`this is an example of a server on port: ${port}`);
+});
